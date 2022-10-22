8812db2c5b6114782cd4138da95b565a63e80e89
adisiofialho-bycoders <adisio.junior@bycoders.com.br>
2022-10-22 19:00:51 -0300

    Validando o calculo do salario liquido de cada funcionario

--- tdd_python/calculadoraSalario.py
+++ tdd_python/calculadoraSalario.py
@@ -8,4 +8,33 @@ class Funcionario:
 
 
 
-  
+  def calculaSalario(cargo, salariobase):
+    if cargo == 'DESENVOLVEDOR':
+      if salariobase >= 3000:
+        desconto = salariobase * 0.20
+        salarioLiquido = salariobase - desconto
+        return salarioLiquido
+      else:
+        desconto = salariobase * 0.10
+        salarioLiquido = salariobase - desconto
+        return salarioLiquido   
+    elif cargo == 'DBA' or cargo == 'TESTADOR':
+      if salariobase >= 2000:
+        desconto = salariobase * 0.20
+        salarioLiquido = salariobase - desconto
+        return salarioLiquido
+      else:
+        desconto = salariobase * 0.10
+        salarioLiquido = salariobase - desconto
+        return salarioLiquido  
+    elif cargo == 'GERENTE':
+      if salariobase >= 5000:
+        desconto = salariobase * 0.20
+        salarioLiquido = salariobase - desconto
+        return salarioLiquido
+      else:
+        desconto = salariobase * 0.10
+        salarioLiquido = salariobase - desconto
+        return salarioLiquido 
+    else:
+      return 'Funcionario não listado'     
--- tdd_python/test_calculadora.py
+++ tdd_python/test_calculadora.py
@@ -11,7 +11,6 @@
 
 
 import unittest
-
 from calculadoraSalario import Funcionario
 #Testando para ver se o salario é um inteiro ou float,
 # caso contrário, der um erro, pois é impossível calcular.
@@ -31,8 +30,20 @@ class Testfuncionario(unittest.TestCase):
   # Adiconado uma função para adiconar antes de todos os Test
   #Para facilitar o códiogo.
   def setUp(self):
-    self.f1 = Funcionario('Adisio', 'Fialho', 'nome@gmail.com', 3200, 'Dev')
+    self.f1 = Funcionario('Adisio', 'Fialho', 'nome@gmail.com', 3200, 'DESENVOLVEDOR')
+    self.dev1 = Funcionario('Adisio1', 'Fialho', 'nome@gmail.com', 2000, 'DESENVOLVEDOR')
+    
     self.f2 = Funcionario('Fabiana', 'Fialho', 'fabiana@gmail.com', 3200.50, 'DBA')
+    self.dba2 = Funcionario('Fabiana1', 'Fialho', 'fabiana@gmail.com', 2000.50, 'DBA')
+
+    self.test1 = Funcionario('Fabiana', 'Fialho', 'fabiana@gmail.com', 3200.50, 'TESTADOR')
+    self.test2 = Funcionario('Fabiana1', 'Fialho', 'fabiana@gmail.com', 2000.50, 'TESTADOR')
+
+    self.gerente1 = Funcionario('Fabiana', 'Fialho', 'fabiana@gmail.com', 5200.50, 'GERENTE')
+    self.gerente2 = Funcionario('Fabiana1', 'Fialho', 'fabiana@gmail.com', 4000.50, 'GERENTE')
+
+    self.rh = Funcionario('Fabiana', 'Fialho', 'fabiana@gmail.com', 1200.50, 'RH')
+    self.cto = Funcionario('Fabiana1', 'Fialho', 'fabiana@gmail.com', 4000.50, 'CTO')
   
   def test_funcionario_attr_nome_tem_o_valor_correto(self):
     self.assertEqual(self.f1.nome, 'Adisio')
@@ -63,11 +74,32 @@ class Testfuncionario(unittest.TestCase):
         self.assertIsInstance(self.f2.salariobase, (int, float))
 
   def test_funcionario_attr_cargo_tem_o_valor_correto(self):
-    self.assertEqual(self.f1.cargo, 'Dev')
+    self.assertEqual(self.f1.cargo, 'DESENVOLVEDOR')
 
   def test_pessoa_attr_cargo_e_str(self):
         self.assertIsInstance(self.f1.cargo, str)
         self.assertIsInstance(self.f2.cargo, str)
+  
+  def test_calcula_salario_dev(self):
+    self.assertEqual(Funcionario.calculaSalario(self.f1.cargo, self.f1.salariobase), 2560)
+    self.assertEqual(Funcionario.calculaSalario(self.dev1.cargo, self.dev1.salariobase), 1800.00)
+
+  def test_calcula_salario_dba(self):
+    self.assertEqual(Funcionario.calculaSalario(self.f2.cargo, self.f2.salariobase), 2560.40)
+    self.assertEqual(Funcionario.calculaSalario(self.dba2.cargo, self.dba2.salariobase), 1600.40)
+
+  def test_calcula_salario_testador(self):
+    self.assertEqual(Funcionario.calculaSalario(self.test1.cargo, self.test1.salariobase), 2560.40)
+    self.assertEqual(Funcionario.calculaSalario(self.test2.cargo, self.test2.salariobase), 1600.40)
+
+  def test_calcula_salario_gerente(self):
+    self.assertEqual(Funcionario.calculaSalario(self.gerente1.cargo, self.gerente1.salariobase), 4160.40)
+    self.assertEqual(Funcionario.calculaSalario(self.gerente2.cargo, self.gerente2.salariobase), 3600.45)
+
+  def test_calcula_salario_nao_listado(self):
+    self.assertEqual(Funcionario.calculaSalario(self.rh.cargo, self.rh.salariobase), 'Funcionario não listado')
+    self.assertEqual(Funcionario.calculaSalario(self.cto.cargo, self.cto.salariobase), 'Funcionario não listado')
+
 
 
 if __name__ == '__main__':
