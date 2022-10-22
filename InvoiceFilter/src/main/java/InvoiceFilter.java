e2d5105d98ad3fb6c2c54aed5d32dd78fef34486
jeron7 <jeronimo.araujo@ccc.ufcg.edu.br>
2022-10-22 13:18:11 -0300

    Add implementation for last filter condition

--- InvoiceFilter/src/main/java/InvoiceFilter.java
+++ InvoiceFilter/src/main/java/InvoiceFilter.java
@@ -1,11 +1,15 @@
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class InvoiceFilter {
+
+    private static final List<State> STATES_FROM_SOUTH = Arrays.asList(State.SC, State.RS,  State.PR);
+
     public static List<Invoice> doFilter(List<Invoice> invoices) {
         if (invoices.isEmpty())
             return Collections.EMPTY_LIST;
@@ -14,14 +18,21 @@ public class InvoiceFilter {
 
         for (Invoice invoice : invoices) {
             boolean isValueLesserThan2000 = invoice.getValue() < 2000;
+
             boolean isValueBetween2000and2500 = invoice.getValue() >= 2000 && invoice.getValue() < 2500;
             boolean isDateEqualOrLesserThanLastMonth = invoice.getDate().compareTo(getLastMonthFromToday()) <= 0;
+
             boolean isValueBetween2500and3000 = invoice.getValue() >= 2500 && invoice.getValue() <= 3000;
             Date twoMonthsAgoFromToday = getNMonthsAgoFromDate(2, new Date());
             boolean isDateLesserOrEqualThanTwoMonthsAgo = invoice.getClient().getInclusionDate().compareTo(twoMonthsAgoFromToday) <= 0;
+
+            boolean isValueGreaterThan4000 = invoice.getValue() > 4000;
+            boolean isClientStateFromSouth = STATES_FROM_SOUTH.contains(invoice.getClient().getState());
+
             if (isValueLesserThan2000
                     || isValueBetween2000and2500 && isDateEqualOrLesserThanLastMonth
                     || isValueBetween2500and3000 && isDateLesserOrEqualThanTwoMonthsAgo
+                    || isValueGreaterThan4000 && isClientStateFromSouth
             ) {
                 result.remove(invoice);
             }
