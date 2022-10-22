9a170e4b65f28fce682738fb6be2aab296f7d5e4
adisiofialho-bycoders <adisio.junior@bycoders.com.br>
2022-10-22 15:52:03 -0300

    Validando se os campos de funcionario, são respectivamente correto

--- tdd_python/test_calculadora.py
+++ tdd_python/test_calculadora.py
@@ -32,21 +32,42 @@ class Testfuncionario(unittest.TestCase):
   #Para facilitar o códiogo.
   def setUp(self):
     self.f1 = Funcionario('Adisio', 'Fialho', 'nome@gmail.com', 3200, 'Dev')
+    self.f2 = Funcionario('Fabiana', 'Fialho', 'fabiana@gmail.com', 3200.50, 'DBA')
   
   def test_funcionario_attr_nome_tem_o_valor_correto(self):
     self.assertEqual(self.f1.nome, 'Adisio')
   
+  def test_pessoa_attr_nome_e_str(self):
+        self.assertIsInstance(self.f1.nome, str)
+        self.assertIsInstance(self.f2.nome, str)
+
   def test_funcionario_attr_sobrenome_tem_o_valor_correto(self):
     self.assertEqual(self.f1.sobrenome, 'Fialho')
   
+  def test_pessoa_attr_nome_e_str(self):
+        self.assertIsInstance(self.f1.sobrenome, str)
+        self.assertIsInstance(self.f2.sobrenome, str)
+
   def test_funcionario_attr_email_tem_o_valor_correto(self):
     self.assertEqual(self.f1.email, 'nome@gmail.com')
   
+  def test_pessoa_attr_email_e_str(self):
+        self.assertIsInstance(self.f1.email, str)
+        self.assertIsInstance(self.f2.email, str)
+
   def test_funcionario_attr_salariobase_tem_o_valor_correto(self):
     self.assertEqual(self.f1.salariobase, 3200)
 
+  def test_pessoa_attr_salariobase_e_int_or_float(self):
+        self.assertIsInstance(self.f1.salariobase, (int, float))
+        self.assertIsInstance(self.f2.salariobase, (int, float))
+
   def test_funcionario_attr_cargo_tem_o_valor_correto(self):
     self.assertEqual(self.f1.cargo, 'Dev')
+
+  def test_pessoa_attr_cargo_e_str(self):
+        self.assertIsInstance(self.f1.cargo, str)
+        self.assertIsInstance(self.f2.cargo, str)
 
 
 if __name__ == '__main__':
