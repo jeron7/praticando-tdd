9c69df927ebceb4b5a30b1cfeed7f8a0cf5e6f4a
jeron7 <jeronimo.araujo@ccc.ufcg.edu.br>
2022-10-22 10:51:37 -0300

    WIP: Add filter empty list case scenario

--- InvoiceFilter/src/main/java/InvoiceFilter.java
+++ InvoiceFilter/src/main/java/InvoiceFilter.java
@@ -1,5 +1,7 @@
 import java.util.List;
 
 public class InvoiceFilter {
-
+    public static List<Invoice> doFilter(List<Invoice> invoices) {
+        return null;
+    }
 }
--- InvoiceFilter/src/test/java/InvoiceFilterTest.java
+++ InvoiceFilter/src/test/java/InvoiceFilterTest.java
@@ -1,7 +1,20 @@
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @DisplayName("Test invoice filter class")
 public class InvoiceFilterTest {
+
+    @Test
+    @DisplayName("When invoices list is empty, should return empty list")
+    void filterEmptyList() {
+        List<Invoice> emptyList = Collections.emptyList();
+
+        assertThat(InvoiceFilter.doFilter(emptyList))
+                .isEqualTo(emptyList);
+    }
 }
