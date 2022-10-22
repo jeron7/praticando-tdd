7d9fad49ccd31dffcc805e15fee009f96f736914
jeron7 <jeronimo.araujo@ccc.ufcg.edu.br>
2022-10-22 10:43:57 -0300

    Add initial project files

---
+++ InvoiceFilter/src/test/java/InvoiceFilterTest.java
@@ -0,0 +1,7 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Test invoice filter class")
+public class InvoiceFilterTest {
+}
